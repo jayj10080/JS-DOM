69b828214ae9e1bcb52905fd72fa24b7330a5637
jay johnson <jayj10080@gmail.com>
2018-05-12 01:42:57 +0000

    Added routes and views for home and ex1.  Added controller.

---
+++ app/controllers/examples_controller.rb
@@ -0,0 +1,7 @@
+class ExamplesController < ApplicationController
+  def home
+  end
+  
+  def ex1
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'examples#home'
+  get 'ex1', to: 'examples#ex1'
 end
